ccc1d0810366ec1759fcee1eb9baffa4852d31d7
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-01 22:08:40 +0300

    Render missions page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Rockets from './views/Rockets';
+import Missions from './views/Missions';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Rockets />} />
+      <Route path="rockets" element={<Rockets />} />
+      <Route path="missions" element={<Missions />} />
     </Routes>
   );
 }
