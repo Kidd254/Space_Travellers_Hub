13ff3b2246a236d4f9138b6a6aa4ff662143be9e
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-01 10:30:09 +0300

    Create rocketsSlice to retrieve rockets data

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,72 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getRockets = createAsyncThunk(
+    'rockets/getRockets',
+    async (thunkApi) => {
+        try {
+            const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+            return response.data;
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message);
+        }
+    }
+);
+
+const initialState = {
+    rockets: [],
+    status: null,
+    error: null,
+};
+
+const rocketsSlice = createSlice({
+    name: 'rockets',
+    initialState,
+    reducers: {
+        addReservedRocket: (state, action) => {
+          const newRocketState = state.rockets.map((rocket) => {
+            if (rocket.rocket_id === action.payload) {
+              return { ...rocket, reserved: true };
+            }
+            return rocket;
+          });
+          state.rockets = newRocketState;
+        },
+        LeaveRocket: (state, action) => {
+          const newRocketState = state.rockets.map((rocket) => {
+            if (rocket.rocket_id === action.payload) {
+              return { ...rocket, reserved: false };
+            }
+            return rocket;
+          });
+          state.rockets = newRocketState;
+        },
+      },
+      extraReducers: (builder) => {
+        builder
+          .addCase(getRockets.pending, (state) => {
+            if (state.rockets.length === 0) state.isLoading = true;
+          })
+          .addCase(getRockets.fulfilled, (state, action) => {
+            if (state.rockets.length === 0) {
+              state.isLoading = false;
+              state.rockets = action.payload.map((rocket) => (
+                {
+                  rocket_id: rocket.rocket_id,
+                  rocket_name: rocket.rocket_name,
+                  description: rocket.description,
+                }
+              ));
+            }
+          })
+          .addCase(getRockets.rejected, (state, action) => {
+            if (state.rockets.length === 0) {
+              state.isLoading = false;
+              state.error = action.payload;
+            }
+          });
+      },
+    });
+
+export const { addReservedRocket, LeaveRocket } = rocketsSlice.actions;
+export default rocketsSlice.reducer;
